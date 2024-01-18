617b1df088359c71e736d48be3f5434ae08cf2b5
NatalieR89 <natalieritch89@gmail.com>
2024-01-18 20:59:48 +0000

    4hw

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-
+/*prints a line*/
 int main(void)
 {
-  
-  puts("Programming is like building a multilingual puzzle,\n");
+
+  puts("Programming is like building a multilingual puzzle\n");
 
   return (0);
 
