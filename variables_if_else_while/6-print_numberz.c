81667264d9eea746bf28ef04110d77c54acc3001
NatalieR89 <natalieritch89@gmail.com>
2024-01-20 04:52:09 +0000

    6cv

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints num 0-9
+ *
+ * Return: Always (Success
+*/
+
+int main(void)
+{
+  int d;
+
+  for (d = '0'; d <= '9'; d++)
+    {
+      putchar(d);
+    }
+
+  putchar('\n');
+
+  return (0);
+}
