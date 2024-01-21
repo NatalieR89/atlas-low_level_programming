9c05f920fe202b8889df6d292fd09750607fa575
NatalieR89 <natalieritch89@gmail.com>
2024-01-21 22:08:18 +0000

    0fnl

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,29 @@
+#include <main.h>
+
+/**
+ * prints '_putchar' using _putchar
+ *
+ * Return: Always
+*/
+
+int _putchar(char c);
+
+int main()
+{
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _purchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
+  _putchar('\n');
+
+  return (0)
+}
+
+int _putchar(char c)
+{
+  return write(1, &c, 1);
+}
