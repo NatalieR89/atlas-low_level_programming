c71a37ed03aa781de1ea7585a5c072a4eeba4c15
NatalieR89 <natalieritch89@gmail.com>
2024-01-21 22:22:43 +0000

    0fnl

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -13,17 +13,12 @@ int main()
   _putchar('_');
   _putchar('p');
   _putchar('u');
-  _purchar('t');
+  _putchar('t');
   _putchar('c');
   _putchar('h');
   _putchar('a');
   _putchar('r');
   _putchar('\n');
 
-  return (0)
-}
-
-int _putchar(char c)
-{
-  return write(1, &c, 1);
+  return (0);
 }
