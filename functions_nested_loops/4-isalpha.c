ca3bf918c89265773d71d1a8a3def9b858f67d78
NatalieR89 <natalieritch89@gmail.com>
2024-01-22 06:14:36 +0000

    4fnl

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks for alphabet char
+ * @c: The letter to be checked
+ *
+ * Return: 1 for corrct char of 0 for anthing else
+*/
+int _isalpha(int c)
+{
+  if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+    {
+      return (1);
+    }
+  return (0);
+}
