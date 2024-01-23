dc3a56110935d92889b9f4f580583816a3186c1b
NatalieR89 <natalieritch89@gmail.com>
2024-01-23 01:30:59 +0000

    9fnl

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,11 +8,13 @@
 void times_table(void)
 {
   int x, y, z, u, d;
+
   for (x = 0; x <= 9; x++)
     {
-      for (y = 0; x <= 9; y++)
+      for (y = 0; y <= 9; y++)
 	{
 	  z = x * y;
+	  
 	  if (z > 9)
 	    {
 	      u = z % 10;
