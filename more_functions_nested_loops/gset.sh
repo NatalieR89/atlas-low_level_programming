2f3b8216055642713731264d1b07c810e836c3e8
NatalieR89 <natalieritch89@gmail.com>
2024-01-27 05:46:00 +0000

    "gconfig"

--- more_functions_nested_loops/gset.sh
+++ more_functions_nested_loops/gset.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-git clone https://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@github.com/NatalieR89/atlas-low_level_programming.git
-
-sleep 2
-
 git config --global user.email "natalieritch89@gmail.com"
 git config --global user.name "NatalieR89"
