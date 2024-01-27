2174b64b887faf57ac072fdaf8e33ef3561332b8
NatalieR89 <natalieritch89@gmail.com>
2024-01-27 06:36:32 +0000

    "3mfnl"

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * void p_n(void) - print num 0-9
+ *
+ * Return: num 0-9
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar(n + '0');	
+	}
+
+	_putchar('\n');
+}
