f2f0b50c4760becb763a77cd92dc515c11faa530
NatalieR89 <natalieritch89@gmail.com>
2024-01-27 00:18:38 +0000

    "2mfnl"

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * int mul - multiplies two numbers
+ * @a: num to multiply
+ * @b: num that multiplies 'a'
+ *
+ * Return: value of 'a'*'b'
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
