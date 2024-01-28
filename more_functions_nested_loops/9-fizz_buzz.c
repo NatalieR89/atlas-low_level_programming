f9b37132db27b011a53058148301963749c62757
NatalieR89 <natalieritch89@gmail.com>
2024-01-28 02:46:03 +0000

    "9mfnl"

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - runs FizzBuzz program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	for (int d = 1; d <= 100; d++)
+	{
+		if (d % 3 == 0 && d % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (d % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (d % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf ("%d ", d);
+		}
+	}
+
+	printf("\n");
+	return 0;
+}
