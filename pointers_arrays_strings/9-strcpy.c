815d4316cda149f539f2290a52490195e5ed706f
NatalieR89 <natalieritch89@gmail.com>
2024-01-30 02:01:46 +0000

    "9pas"

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,8 +1,21 @@
 #include "main.h"
 
 /**
+ * _strcpy - Copy string
+ * @dest: where to be copied
+ * @src: the value of scr
  *
- *
- *
- *
+ * Return: the dest pointer
  */
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src [a] != '\0'; a++)
+	{
+		dest[a] = src[a];
+	}
+	dest[a++] = '\0';
+
+	return dest;
+}
