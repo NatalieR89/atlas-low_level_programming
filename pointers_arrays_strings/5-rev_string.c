88508762db1143e1e366c3ff83c26bbcdc5ec976
NatalieR89 <natalieritch89@gmail.com>
2024-01-30 00:21:09 +0000

    "5pas"

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: String to be modified
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int a, b, c;
+	char *d, temp;
+
+	d = s;
+
+	while (s[b] != '\0')
+	{
+		b++;
+	}
+
+	for (c = 1; c <= b; c++)
+	{
+		d++;
+	}
+
+	for (a = 0; a <= (b / 2); a++)
+	{
+		temp = s[a];
+		s[a] = *d;
+		*d = temp;
+		d--;
+	}
+}
