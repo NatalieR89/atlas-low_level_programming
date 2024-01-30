f792d18cbd70488d299f169e6915ba385a1b4963
NatalieR89 <nataliritch89@gmail.com>
2024-01-30 05:01:04 +0000

    "10pas"

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,18 +10,18 @@ int _atoi(char *s)
 {
 	unsigned int cnt = 0;
 	unsigned int sz = 0;
-	unsigned int a = 1;
+	unsigned int a = 0;
 	unsigned int b = 1;
 	unsigned int c = 1;
 	unsigned int d;
 
 	while (*(s + cnt) != '\0')
 	{
-		if (sz > 0 &&(*(s + cnt) < '0' || (*(s + cnt > '9'))))
+		if (sz > 0 && (*(s + cnt) < '0' || *(s + cnt) > '9'))
 			break;
 		if (*(s + cnt) == '-')
 			b *= -1;
-		if ((*(s + cnt >= '0') && (*(s + cnt) <= '9')))
+		if ((*(s + cnt) >= '0') && (*(s + cnt) <= '9'))
 			{
 				if (sz > 0)
 				c *= 10;
