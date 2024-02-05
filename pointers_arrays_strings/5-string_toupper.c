c0ad36b0afb073b48c5fc3b985948c6ad69c2097
NatalieR89 <natalieritch89@gmail.com>
2024-02-05 07:57:52 +0000

    "5mpas"

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase to uppercase
+ * @*: string to be modified
+ *
+ * Return: chacter value
+ */
+char *string_toupper(char *str)
+{
+	char *p = str;
+
+	while (*p != '\0')
+	{
+		*p = toupper((unsigned char)*p);
+		p++;
+	}
+
+	return (str);
+}
