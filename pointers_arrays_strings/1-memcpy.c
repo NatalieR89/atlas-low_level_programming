eac5ae03c76f02c4ad7ba0ae2f2c968868085421
NatalieR89 <natalieritch89@gmail.com>
2024-02-06 02:43:31 +0000

    "1empas"

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,10 +1,12 @@
 #include "main.h"
 #include <stddef.h>
 /**
+ * _memcpy - memory area to be copied
+ * @dest: destination memory
+ * @src: source memory
+ * @n: bytes to copy
  *
- *
- *
- *
+ * Return: dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
