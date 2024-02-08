9731a4ea265b817d035ba4ff7bd578f884cf0280
NatalieR89 <natalieritch89@gmail.com>
2024-02-08 01:01:11 +0000

    "1recur"

---
+++ recursion/1-pring_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	_puts_recursion(s - 1);
+}
