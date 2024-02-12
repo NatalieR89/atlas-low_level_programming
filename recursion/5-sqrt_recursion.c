d48cf412fbc539ca9c21b2827281db4f8b859baa
NatalieR89 <natalieritch89@gmail.com>
2024-02-12 05:41:40 +0000

    "5recur"

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+int _sqrt_helper(int n, int guess) 
+{
+    if (guess * guess == n) 
+    {
+        return guess;
+    } 
+    else if (guess * guess > n) 
+    {
+        return -1;
+    } 
+    else 
+    {
+        return _sqrt_helper(n, guess + 1);
+    }
+}
+
+int _sqrt_recursion(int n) 
+{
+    if (n < 0) 
+    {
+        return -1;
+    } 
+    else if (n == 0 || n == 1) 
+    {
+	    return n;
+    } 
+    else 
+    {
+	return _sqrt_helper(n, 1);
+    }
+}
