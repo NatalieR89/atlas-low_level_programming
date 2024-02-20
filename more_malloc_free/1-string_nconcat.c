a24b5a2f54cc8c5d4d1f69a4612cf304cb0e2681
NatalieR89 <natalieritch89@gmail.com>
2024-02-20 04:32:38 +0000

    "1mmf"

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+    char *new_str;
+    unsigned int i, j, len_s1 = 0, len_s2 = 0;
+
+    if (s1 == NULL)
+        s1 = "";
+    if (s2 == NULL)
+        s2 = "";
+
+    while (s1[len_s1])
+        len_s1++;
+    while (s2[len_s2])
+        len_s2++;
+
+    if (n >= len_s2)
+        n = len_s2;
+
+    new_str = malloc(sizeof(char) * (len_s1 + n + 1));
+    if (new_str == NULL)
+        return (NULL);
+
+    for (i = 0; i < len_s1; i++)
+        new_str[i] = s1[i];
+    for (j = 0; j < n; j++, i++)
+        new_str[i] = s2[j];
+
+    new_str[i] = '\0';
+
+    return (new_str);
+}
