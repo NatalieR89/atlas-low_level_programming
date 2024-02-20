54b4c4b34c7d8803ef4b505d0097663da4e691f4
NatalieR89 <natalieritch89@gmail.com>
2024-02-20 01:18:19 +0000

    "3acav"

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,14 +12,13 @@
 int main(int argc, char *argv[])
 {
 	int n1 = 0, n2 = 0;
-	int result = (n1 * n2);
 
 	if (argc == 3)
 	{
 		n1 = atoi(argv[1]);
 		n2 = atoi(argv[2]);
 
-		printf("%d\n", result);
+		printf("%d\n", n1 * n2);
 	}
 	else
 	{
