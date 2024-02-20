88ccf01376e99189584aef348fa741b58af0779b
NatalieR89 <natalieritch89@gmail.com>
2024-02-20 04:49:34 +0000

    "2mmf"

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,25 +1,28 @@
+#include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * _calloc - Allocates memory for an array
  * @nmemb: Number of elements
  * @size: Size of each element
  *
- * Return: Pointer to the allocated memory
+ * Return: Pointer to the allocated memory, or NULL if nmemb or size is 0 or if
+ * malloc fails
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    void *ptr;
+    char *p;
+    unsigned int i;
 
     if (nmemb == 0 || size == 0)
         return (NULL);
 
-    ptr = malloc(nmemb * size);
-    if (ptr == NULL)
+    p = malloc(nmemb * size);
+    if (p == NULL)
         return (NULL);
 
-    memset(ptr, 0, nmemb * size);
+    for (i = 0; i < nmemb * size; i++)
+        p[i] = 0;
 
-    return (ptr);
+    return (p);
 }
