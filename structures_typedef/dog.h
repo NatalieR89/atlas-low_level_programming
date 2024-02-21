291caeeca6d77fb2821ff8f0e7110b9c4a7c1506
NatalieR89 <natalieritch89@gmail.com>
2024-02-21 22:07:08 +0000

    "0str"

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * struct dog - structure describing a dog
+ * @name: Name of the dog
+ * @age:  Age of the dog
+ * @owner: Owners name
+ */
 struct dog
 {
 	char *name;
