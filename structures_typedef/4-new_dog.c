2de5fbd4705e283f71eb35852c07bd61af79b95c
NatalieR89 <natalieritch89@gmail.com>
2024-02-23 18:39:16 +0000

    "4str"

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -5,7 +5,12 @@ int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 
 /**
+ * newDog - ...
+ * @name: Name of dog
+ * @age: age of dog
+ * @owner: Name of the owner
  *
+ * Return: new dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
