65154ad0fcf97027764e95f1f8faf0418155001b
NatalieR89 <natalieritch89@gmail.com>
2024-06-10 18:09:10 +0000

    "4args"

---
+++ argc_argv/4-add.c
@@ -0,0 +1,42 @@
+
+/**
+  * main - Prints the sum of positive numbers
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: zero
+  */
+int main(int argc, char *argv[])
+{
+	int d;
+	unsigned int a, sum = 0;
+	char *b;
+
+	if (argc > 1)
+	{
+		for (d = 1; d < argc; d++)
+		{
+			b = argv[i];
+
+			for (a = 0; a < strlen(b); a++)
+			{
+				if (b[a] < 48 || b[b] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			sum += atoi(b);
+			b++;
+		}
+
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("0\n");
+	}
+
+	return (0);
+}
